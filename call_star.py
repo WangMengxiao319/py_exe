21f213046d0a1f89c1cdca6f16fd354caa93f0fb
WangMengxiao319 <17863102805@163.com>
2022-03-31 12:56:36 +0800

    20220331

---
+++ call_star.py
@@ -0,0 +1,7 @@
+from star import pstar
+import star
+
+print(star.hi)
+pstar(3)
+star.pstar(4)
+star.astar(5) # 这个就不能被执行
---
+++ star.py
@@ -0,0 +1,7 @@
+hi = 'hello world!'
+def pstar(n):
+    print('*'*n)
+
+if __name__=='__main__':
+    def astar(n):
+        print('&'*n)
